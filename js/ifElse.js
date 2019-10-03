ce8868e4bce06c956f191f57d2299130790ccc92
Verenasworld <verena.felizidas@gmail.com>
2019-10-03 14:54:19 +0200

    add a condition else with a alert expenditure ( alert Box Ausgabe wenn die Bedingung nicht erfüllt ist. Wenn/ If Dann/ Else)

--- js/ifElse.js
+++ js/ifElse.js
@@ -1,7 +1,7 @@
 "use strict";
 var ausgabe = document.getElementById("ausgabe");
         rechne("+", 3, 5);
-        rechne("-", 7, 3);
+        rechne("/", 7, 3);
 
         function rechne( operator, x, y) {
             var ergebnis;
@@ -15,6 +15,13 @@ var ausgabe = document.getElementById("ausgabe");
                 ergebnis = x - y;
             }
 
-            ausgabe.innerHTML += x + operator + y + " = " + ergebnis + "<br/>";
+           if ( operator == "+" || operator == "-"){
+               console.log("alles klar.")}
+           else {
+
+               alert(" es sind nur + und - erlaubt");
+            }
+
+         ausgabe.innerHTML += x + operator + y + " = " + ergebnis + "<br/>"
 
         }
