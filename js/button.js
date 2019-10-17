213c1af1b5589be3ecbc2845aa6a38bf04307e21
Verenasworld <verena.felizidas@gmail.com>
2019-10-17 21:32:09 +0200

    add Button

--- js/button.js
+++ js/button.js
@@ -1,29 +1,35 @@
 "use strict";
 
 var ausgabe = document.getElementById("ausgabe");
-
 var addieren = document.getElementById("addieren");
-var subtrahieren = document.getElementById("subtrahieren");
-var a= 3;
+var substrahieren = document.getElementById("subtrahieren");
+var a= 8;
 var b= 5;
+eingabe.innerHTML = a + " und " + b;
 
-eingabe.innerHTML = a + "und" + b;
 
-addieren.addEventListener("click" , function()){
+
+addieren.addEventListener("click" , function(){
     rechne("+" ,a,b);
-})
+});
 
-subtrahieren.addEventListener("click", subtrahieren);
+substrahieren.addEventListener("click",subtrahieren);
 
 function subtrahieren() {
-    rechne("-",a, b);
-
+    rechne("-",a,b);
 }
+
 function rechne( operator, x, y) {
     var ergebnis;
-    if (operator == "+"){
-        ergebnis = x + y ;
+    if (operator == "+") {
+        ergebnis = x + y;
     }
-
+        if (operator == "-"){
+            ergebnis = x - y;
+        }
+        ausgabe.innerHTML = x + operator + y + "=" + ergebnis + "<br/>";
 }
+
+
+
 
