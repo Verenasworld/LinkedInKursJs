8b9373c8f8f894794e1041d6f61dc964c7bfa846
Verenasworld <verena.felizidas@gmail.com>
2019-10-30 16:59:35 +0100

    fix bug´s

--- js/GalleryImgOverlay/bigPicture.js
+++ js/GalleryImgOverlay/bigPicture.js
@@ -1,66 +1,64 @@
+
+
 var images = new Array();
 var actualBigImage = 0;
 var bigImage = document.querySelector("#bigimage");
 var overlay = document.querySelector("#overlay");
 
 var imgNumber = 0;
-
-
-
-Array.prototype.forEach.call(document.querySelectorAll(".bild"), function (element) {
-    element.setAttribute("date-number", imgNumber++);
+Array.prototype.forEach.call(document.querySelectorAll(".bild"), function( element ) {
+    element.setAttribute("data-number", imgNumber++);
     var style = window.getComputedStyle(element, false);
-    images.push(style.backgroundImage.slice(4, -1).replace(/"/g, ""));
-    element.addEventListener("click", function () {
-        showOverlay(this);
+    images.push( style.backgroundImage.slice( 4, -1).replace(/"/g,"") );
+
+    element.addEventListener("click", function(){
+        showOverlay( this );
     })
 });
 
-document.getElementById("closeOverlayButton").addEventListener('click', closeOverlay);
+document.getElementById("closeOverlayButton").addEventListener('click', closeOverlay );
 
-document.addEventListener('keydown', function (event)
-{ switch( event.key ){
-    case 32: case 39: // Leertaste, rechts
-        actualBigImage++;
-        break;
-    case 37: // links
-        actualBigImage--;
-        break;
-    case 27: // ESC
-        closeOverlay();
-        break;
-    default:
-        console.log( event.key);
-}
+document.addEventListener('keydown', function(event){
+    switch( event.keyCode ){
+        case 32: case 39: // Leertaste, rechts
+            actualBigImage++;
+            break;
+        case 37: // links
+            actualBigImage--;
+            break;
+        case 27: // ESC
+            closeOverlay();
+            break;
+        default:
+            console.log(  event.keyCode);
+    }
 
     if( actualBigImage === images.length ) actualBigImage = 0;
     if( actualBigImage === -1 ) actualBigImage = images.length - 1;
 
     showBigImage();
 });
-    bigImage.addEventListener("click", function (event) {
-        actualBigImage++;
 
-        actualBigImage %= images.length;
-        showBigImage();
+bigImage.addEventListener("click", function( event){
+    actualBigImage++;
 
-    })
+    actualBigImage %= images.length;
+    showBigImage();
+})
 
-    bigImage.addEventListener("dblclick", closeOverlay);
+bigImage.addEventListener("dblclick", closeOverlay);
 
-    function showBigImage() {
-        bigImage.setAttribute("src", images[actualBigImage]);
+function showBigImage(){
+    bigImage.setAttribute("src", images[ actualBigImage ]);
+}
 
-    }
-    function showOverlay(element) {
-        actualBigImage = element.dataset.number;
-        bigImage.setAttribute("src", images[actualBigImage]);
-        overlay.setAttribute("style", "display:block");
+function closeOverlay(){
+    overlay.setAttribute("style","display:none;");
+}
 
-    }
-
-    function closeOverlay() {
-        overlay.setAttribute("style", "display:none;");
-
+function showOverlay( element ){
+    actualBigImage = element.dataset.number;
+    bigImage.setAttribute("src", images[ actualBigImage ]);
+    overlay.setAttribute("style","display:block;");
 }
 
